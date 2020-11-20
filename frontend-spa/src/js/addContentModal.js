60b28cde736b5e6be10df20014dab7699b7caa51
Pete Schaaf <peteschaaf.19@gmail.com>
2020-11-20 10:34:21 -0500

    Changes to start with modal functionality, almost on the wrong branch, oops

---
+++ frontend-spa/src/js/addContentModal.js
@@ -0,0 +1,40 @@
+const addContentModal = function()
+
+const developerButton = document.querySelector(".add-song-button");
+const modal = document.querySelector(".modal");
+const backdrop = document.querySelector(".backdrop");
+
+function closeModal() {
+    modal.style.display = "none";
+    backdrop.style.display = "none";
+}
+developerButton.addEventListener("click", function() {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+});
+
+const exitButton = document.querySelector(".exit");
+exitButton.addEventListener("click", closeModal);
+backdrop.addEventListener("click", closeModal);
+
+export {
+    addContentModal
+}
+
+// About the Developers Modal
+// const developerButton = document.querySelector(".about-button");
+// const modal = document.querySelector(".modal");
+// const backdrop = document.querySelector(".backdrop");
+
+// function closeModal() {
+//     modal.style.display = "none";
+//     backdrop.style.display = "none";
+// }
+// developerButton.addEventListener("click", function() {
+//     modal.style.display = "block";
+//     backdrop.style.display = "block";
+// });
+
+// const exitButton = document.querySelector(".exit");
+// exitButton.addEventListener("click", closeModal);
+// backdrop.addEventListener("click", closeModal);
